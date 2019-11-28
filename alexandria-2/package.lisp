cc39718f973d23653acfcab8db2ebcb9f030c702
Philipp Marek <philipp@marek.priv.at>
2019-11-28 12:26:32 +0100

    First part of alexandria-2 - the package.

---
+++ alexandria-2/package.lisp
@@ -0,0 +1,8 @@
+(defpackage :alexandria-2
+  (:nicknames :alexandria.2 :alex-2)
+  (:use :cl :alexandria.1.0.0)
+  #+sb-package-locks
+  (:lock t)
+  (:export .
+    #. (let (res) (do-external-symbols (sym :alexandria.1.0.0) (push sym res)) res)
+   ))
