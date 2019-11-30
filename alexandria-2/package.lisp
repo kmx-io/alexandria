2e3994270c9dee76f3c8141d8672c37661c9494d
Andrea Richiardi <a.richiardi.work@gmail.com>
2019-11-29 19:04:04 -0800

    Introduce line-up-first and line-up-last macros
    This patch introduces the line-up-first and line-up-last macros in
    control-flow.  The macros have been popularized by Clojure and afterwards
    adopted in Emacs Lisp (see thread-first and thread-last in subr-x.el).
    
    This implementation is a port of the latter, where we use a common
    line-up-iter(ative) tail recursive function for computing the results.  The
    choice of the full name was made in the spirit of the library, conservatively
    avoiding new syntax like -> or ->>.

--- alexandria-2/package.lisp
+++ alexandria-2/package.lisp
@@ -3,7 +3,9 @@
   (:use :cl :alexandria.1.0.0)
   #+sb-package-locks
   (:lock t)
-  (:export 
-    #:delete-from-plist*
+  (:export
+   #:delete-from-plist*
+   #:line-up-first
+   #:line-up-last
     . #. (let (res) (do-external-symbols (sym :alexandria.1.0.0) (push sym res)) res)
    ))
--- alexandria-2/tests.lisp
+++ alexandria-2/tests.lisp
@@ -19,3 +19,108 @@
   ((b 2 d 4 d 5)
    ((c . 3) (a . 1))))
 
+
+;; Control Flow tests
+
+(deftest line-up-first.no-form
+    (values
+     (equal (macroexpand '(line-up-first 5))
+            5)
+     (equal (macroexpand '(line-up-first (+ 1 2)))
+            '(+ 1 2)))
+  t
+  t)
+
+(deftest line-up-first.function-names-are-threaded
+    (values
+     (equal (macroexpand '(line-up-first 5 -))
+            '(- 5))
+     (equal (macroexpand '(line-up-first (+ 1 2) -))
+            '(- (+ 1 2))))
+  t
+  t)
+
+(deftest line-up-first.list-promotion
+    (macroexpand '(line-up-first
+                   5
+                   (+ 20)
+                   (/ 25)
+                   -
+                   (+ 40)))
+  (+ (- (/ (+ 5 20) 25)) 40)
+  t)
+
+(deftest line-up-first.multiple-args
+    (macroexpand '(line-up-first
+                   "this-is-a-string"
+                   (subseq 0 4)))
+  (subseq "this-is-a-string" 0 4)
+  t)
+
+(deftest line-up-first.several-examples
+    (values
+     (equal (line-up-first (+ 40 2)) 42)
+     (equal (line-up-first
+             5
+             (+ 20)
+             (/ 25)
+             -
+             (+ 40)) 39)
+     (equal (line-up-first
+             "this-is-a-string"
+             (subseq 4 5)
+             (string-trim  "--------good"))
+            "good"))
+  t
+  t
+  t)
+
+;; Thread last tests
+
+(deftest line-up-last.no-forms
+    (values
+     (equal (macroexpand '(line-up-last 5)) 5)
+     (equal (macroexpand '(line-up-last (+ 1 2))) '(+ 1 2)))
+  t
+  t)
+
+(deftest line-up-last.function-names-are-threaded
+    (values (equal (macroexpand
+                    '(line-up-last 5
+                      -))
+                   '(- 5))
+            (equal (macroexpand
+                    '(line-up-last (+ 1 2)
+                      -))
+                   '(- (+ 1 2))))
+  t
+  t)
+
+(deftest line-up-last.lisp-promotion
+    (macroexpand '(line-up-last
+                   5
+                   (+ 20)
+                   (/ 25)
+                   -
+                   (+ 40)))
+  (+ 40 (- (/ 25 (+ 20 5))))
+  t)
+
+(deftest line-up-last.several-examples
+    (values (equal (line-up-last (+ 40 2)) 42)
+            (equal (line-up-last
+                    5
+                    (+ 20)
+                    (/ 25)
+                    -
+                    (+ 40))
+                   39)
+            (equal (line-up-last
+                    (list 1 -2 3 -4 5)
+                    (mapcar #'abs)
+                    (reduce #'+)
+                    (format nil "abs sum is: ~D"))
+                   "abs sum is: 15"))
+  t
+  t
+  t)
